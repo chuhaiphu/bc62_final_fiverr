39ad323712deb994bc4f9697c6a8080245df328e
Doan Vinh Phu <doanvinhphu@Doans-MacBook-Pro.local>
2024-05-29 17:51:19 +0700

    finised--propositionSection

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Carousel from '~/components/Carousel'
 import Header from '~/components/Header'
+import Proposition from '~/components/Proposition'
 export default function Home() {
   return (
     <>
       <Header />
-      <Carousel/>
+      <Carousel />
+      <Proposition />
     </>
   )
 }

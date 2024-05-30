977278154e365fe74036cc0ca0414594ae1d7f64
Doan Vinh Phu <doanvinhphu@Doans-MacBook-Pro.local>
2024-05-30 22:49:26 +0700

    after merge

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,16 +3,15 @@ import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import Category from '~/components/Category';
 import Carousel from '~/components/Carousel'
-import Header from '~/components/Header'
 import Proposition from '~/components/Proposition'
 export default function Home() {
   return (
     <>
       <Header />
-      <Category/>
-      <Footer/>
       <Carousel />
       <Proposition />
+      <Category/>
+      <Footer/>
     </>
   )
 }

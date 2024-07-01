e52417ef5f8647179a4de63dd30f46f769cc6ec0
Doan Vinh Phu <doanvinhphu@Doans-MacBook-Pro.local>
2024-07-02 00:02:54 +0700

    finished --add comment

--- src/components/JobDetail/Add-Comment/index.tsx
+++ src/components/JobDetail/Add-Comment/index.tsx
@@ -1,50 +1,54 @@
 import { Box, Button, Rating } from "@mui/material";
 import { useState } from "react";
-import Swal from "sweetalert2";
 import { useAddComment } from "~/hooks/addComment-hook";
-import { useUserStore } from "~/store/user-store"
+import { useCommentStore } from "~/store/comment-store";
+import { useUserStore } from "~/store/user-store";
+
 
 
 export default function AddComment({ props }: any) {
-    // const user = useUserStore((state) => state.user);
-    const user = { avatar: 221, id: 1 }
-    
-    const [value, setValue] = useState<number | null>(5);
-    const { mutate: addComment } = useAddComment( );
+    const commentElement = document.getElementById('comment') as HTMLTextAreaElement;
 
-    
-    const handleComment = () => {
+    const user = useUserStore((state) => state.user);
+    // const user = { avatar: 221, id: 1 }
+
+    const [value, setValue] = useState<number | null>(5);
+    const { mutate: addComment } = useAddComment(props);
+    const refetchComments = useCommentStore((state) => state.refetchComments);
+
+    const handleComment = async () => {
         const dataComment = {
-            maNguoiBinhLuan: user?.id,
+            maNguoiBinhLuan: user?.id!,
             noiDung: (document.getElementById('comment') as HTMLInputElement)?.value ?? '',
             ngayBinhLuan: new Date().toLocaleDateString(),
-            saoBinhLuan: value! , 
+            saoBinhLuan: value!,
             maCongViec: props,
         };
         console.log(dataComment);
-        
-        addComment(dataComment);
-        
+
+        await addComment(dataComment);
+        refetchComments();
+        commentElement.value = '';
     }
     return (
         (user !== null) ? (
             <Box display='flex' flexDirection='column'>
-                <Box   style={{margin:'0 0 1vh 3vh', display: 'flex' ,alignItems:'center'}}>
-                    <p style={{paddingTop: '2vh'}}>Đánh giá :</p>
+                <Box style={{ margin: '0 0 1vh 3vh', display: 'flex', alignItems: 'center' }}>
+                    <p style={{ paddingTop: '2vh' }}>Đánh giá :</p>
                     <Rating
-                    style={{ margin: '2vh 0 0 2vh' }}
-                    name="simple-controlled"
-                    value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                />
+                        style={{ margin: '2vh 0 0 2vh' }}
+                        name="simple-controlled"
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    />
                 </Box>
                 <Box display='flex'>
                     {user?.avatar}
                     <textarea id="comment" name="comment" placeholder="Nhập bình luận của bạn..." style={{ width: '70%' }}></textarea>
                 </Box>
-                <Button onClick={handleComment} style={{ width:'15vw', margin: '2vh 0 0 2vh' }} variant="contained">Add Comment</Button>
+                <Button onClick={handleComment} style={{ width: '15vw', margin: '2vh 0 0 2vh' }} variant="contained">Add Comment</Button>
             </Box>
         ) : (
             <Box >
--- src/hooks/addComment-hook.ts
+++ src/hooks/addComment-hook.ts
@@ -1,10 +1,14 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { addCommentApi } from "~/apis/add-comment-api"
 
-export const useAddComment = (onSuccess?: () => void, onError?: (error: Error) => void) => {
-    return useMutation({
-      mutationFn: addCommentApi,
-      onSuccess: onSuccess,
-      onError: onError
-    })
-  }
+export const useAddComment = (maCongViec: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: addCommentApi,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['get-comment', maCongViec] });
+    },
+  })
+}
